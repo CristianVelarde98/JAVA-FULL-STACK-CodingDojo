d4f3892107bbd8d6453c226dec2f91b9d1bd3d8d
CristianVelarde98 <cristianvelardefx@gmailo.com>
2023-07-12 21:06:38 -0300

    Primer programa Java

---
+++ Tu Propio Programa Java/holaJava.java
@@ -0,0 +1,12 @@
+public class holaJava {
+
+    public static void main(String[] args) {
+        String nombre = "Cristian";
+        Integer edad = 24;
+        String ciudad = "Capital Federal, Arg";
+        
+        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Tengo " + edad);
+        System.out.println("Mi ciudad es " + ciudad);
+    }
+}
